f9fecb05077a600c54b69691a16813f461d56677
Colgr8 <colwhi@mweb.co.za>
2026-06-05 12:31:02 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
- const CACHE = 'forge-v1';
+const CACHE = 'forge-v2';
 const FILES = [
   '/',
   '/index.html',
@@ -32,5 +32,7 @@ self.addEventListener('fetch', e => {
   e.respondWith(
     caches.match(e.request).then(cached => cached || fetch(e.request))
   );
+});
+
 });
 
